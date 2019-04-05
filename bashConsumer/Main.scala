226432535f5565bc33dc573888b75c345c5bdea9
remitejo <37656734+remitejo@users.noreply.github.com>
2019-04-05 06:44:04 +0200

    Update Main.scala

--- bashConsumer/Main.scala
+++ bashConsumer/Main.scala
@@ -15,7 +15,7 @@ object Main extends App {
 
 
   val conf = new SparkConf().setAppName("kafkAlert").setMaster("local[*]")
-  val ssc = new StreamingContext(conf, Seconds(20))//Minutes(10))//
+  val ssc = new StreamingContext(conf, Minutes(10)) //Run every 10mn
   val spark = SparkSession.builder().appName("Spark SQL").getOrCreate()
   import spark.implicits._
   val kafkaParams = Map[String, Object](
